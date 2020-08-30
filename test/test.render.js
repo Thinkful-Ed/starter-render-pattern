cc6526d0d2f6037cd947537dc5940e7b86f391d5
Vincent Ramdhanie <vramdhanie@gmail.com>
2020-08-30 14:46:23 -0400

    Initial test code

---
+++ test/test.render.js
@@ -0,0 +1,63 @@
+const expect = chai.expect;
+
+describe("Render Pattern", function () {
+  describe("renderBook()", function () {
+    before(function () {});
+
+    after(function () {});
+
+    it("should create HTML code for given book", function () {});
+  });
+
+  describe("calculateTotal()", function () {
+    before(function () {});
+
+    after(function () {});
+
+    it("should return 0 when cart is empty", function () {});
+
+    it("should sum prices of single items", function () {});
+
+    it("should sum prices * quantities for multiple items", function () {});
+  });
+
+  describe("render()", function () {
+    before(function () {});
+
+    after(function () {});
+
+    it("should display 'Nothing in cart' when cart is empty", function () {});
+
+    it("should display a total of 0 when nothing in cart", function () {});
+
+    it("should display all items in cart", function () {});
+
+    it("should display total price of items in cart", function () {});
+  });
+
+  describe("sortByPrice()", function () {
+    before(function () {});
+
+    after(function () {});
+
+    it("should result in empty array when array is empty ", function () {});
+
+    it("should sort books in ascending order of price", function () {});
+
+    it("should render the sorted array", function () {});
+
+    it("should be able to click on the sort button to sort", function () {});
+  });
+
+  describe("deleteBook()", function () {
+    before(function () {});
+
+    after(function () {});
+
+    it("should be able to click on a delete button to delete a book", function () {});
+
+    it("should remove the book with given id from the array", function () {});
+
+    it("should render updated array", function () {});
+  });
+});
